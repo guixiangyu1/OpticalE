27630cab1c7e83adfacf728313e9b8ef50c03025
guixiangyu <guixiangyu1@gmail.com>
2019-12-19 14:29:59 +0800

    init

--- run.sh
+++ run.sh
@@ -42,7 +42,7 @@ CUDA_VISIBLE_DEVICES=$GPU_DEVICE python -u $CODE_PATH/run.py --do_train \
     -g $GAMMA -a $ALPHA -adv \
     -lr $LEARNING_RATE --max_steps $MAX_STEPS \
     -save $SAVE --test_batch_size $TEST_BATCH_SIZE \
-    -r $REGU
+    -r $REGU \
     ${15} ${16} ${17} ${18} ${19} ${20}
 
 elif [ $MODE == "valid" ]
