c9d0fcc58033736503cb1d4ddba0c17df60cc5ed
guixiangyu <guixiangyu1@gmail.com>
2019-12-19 14:42:47 +0800

    init

--- run.sh
+++ run.sh
@@ -25,7 +25,6 @@ ALPHA=${10}
 LEARNING_RATE=${11}
 MAX_STEPS=${12}
 TEST_BATCH_SIZE=${13}
-REGU=${14}
 
 if [ $MODE == "train" ]
 then
@@ -42,8 +41,7 @@ CUDA_VISIBLE_DEVICES=$GPU_DEVICE python -u $CODE_PATH/run.py --do_train \
     -g $GAMMA -a $ALPHA -adv \
     -lr $LEARNING_RATE --max_steps $MAX_STEPS \
     -save $SAVE --test_batch_size $TEST_BATCH_SIZE \
-    -r $REGU \
-    ${15} ${16} ${17} ${18} ${19} ${20}
+    ${14} ${15} ${16} ${17} ${18} ${19} ${20}
 
 elif [ $MODE == "valid" ]
 then
