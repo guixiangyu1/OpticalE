eb13c64eb11ec58f8247ce5c2f77c5340dce8216
guixiangyu <guixiangyu1@gmail.com>
2019-12-25 11:43:02 +0800

    abs

--- codes/model.py
+++ codes/model.py
@@ -542,6 +542,7 @@ class KGEModel(nn.Module):
                         batch_size = positive_sample.size(0)
 
                         score = model((positive_sample, negative_sample), mode)
+                        score = torch.sigmoid(score)
                         score += filter_bias
 
                         #Explicitly sort all the entities to ensure that there is no test exposure bias
