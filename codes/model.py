f74c7db1983b0555361d578f4337c114ef7cc42b
guixiangyu <guixiangyu1@gmail.com>
2020-01-06 14:23:01 +0800

    without adv, periodic

--- codes/model.py
+++ codes/model.py
@@ -407,9 +407,9 @@ class KGEModel(nn.Module):
         weight = head_weight + tail_weight
         weight = F.softmax(weight, dim=2)
         if mode == 'tail':
-            assert  weight.shape == tail_weight
+            assert  weight.shape == head_weight
         else:
-            assert weight.shape == head_weight
+            assert weight.shape == tail_weight
 
         score = torch.stack([score1, score2], dim=3) * weight
         score = score.sum(dim=3)
