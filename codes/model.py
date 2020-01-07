0d086225ef5707bf196184c414fddd328232b2b2
guixiangyu <guixiangyu1@gmail.com>
2020-01-07 16:17:18 +0800

    without adv, periodic

--- codes/model.py
+++ codes/model.py
@@ -573,30 +573,21 @@ class KGEModel(nn.Module):
         negative_score = model((positive_sample, negative_sample), mode=mode)
 
         if args.negative_adversarial_sampling:
-            #In self-adversarial sampling, we do not apply back-propagation on the sampling weight
+            # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
             # detach() 函数起到了阻断backpropogation的作用
-            negative_score = (F.softmax(negative_score * args.adversarial_temperature, dim = 1).detach()
-                              * F.logsigmoid(-negative_score)).sum(dim = 1)
+            negative_score = (F.softmax(negative_score * args.adversarial_temperature, dim=1).detach()
+                              * negative_score).sum(dim=1)
         else:
-            negative_score = F.logsigmoid(-negative_score).mean(dim = 1)
+            negative_score = negative_score.mean(dim=1)
 
         # mode = 'single'
         positive_score = model(positive_sample)
-
-        positive_score = F.logsigmoid(positive_score).squeeze(dim = 1)
+        # Returns a tensor with all the dimensions of :attr:`input` of size `1` removed.
+        positive_score = positive_score.squeeze(dim=1)
 
         zeros = torch.zeros(positive_score.size()).cuda()
         margin_loss = torch.max(negative_score + args.gamma - positive_score, zeros)
 
-        if args.uni_weight:
-            loss = margin_loss.mean()
-        else:
-            loss = (subsampling_weight * margin_loss).sum() / subsampling_weight.sum()
-
-        # 这里的weight和self-adversarial 没有任何联系
-        #只不过是一种求负样本loss平均的策略，那就得参考每个样本的重要性了，也就是 subsampling_weight
-        # 这个weight来源于word2vec的subsampling weight，
-        # 这里是在一个batch中，评估每一个样本的权重
         # if args.uni_weight:
         #     positive_sample_loss = - positive_score.mean()
         #     negative_sample_loss = - negative_score.mean()
@@ -604,14 +595,16 @@ class KGEModel(nn.Module):
         #     positive_sample_loss = - (subsampling_weight * positive_score).sum()/subsampling_weight.sum()
         #     negative_sample_loss = - (subsampling_weight * negative_score).sum()/subsampling_weight.sum()
 
-        # loss = (positive_sample_loss + negative_sample_loss)/2
-
+        if args.uni_weight:
+            loss = margin_loss.mean()
+        else:
+            loss = (subsampling_weight * margin_loss).sum() / subsampling_weight.sum()
 
         if args.regularization != 0.0:
-            #Use L3 regularization for ComplEx and DistMult
+            # Use L3 regularization for ComplEx and DistMult
             regularization = args.regularization * (
-                model.entity_embedding.norm(p = 3)**3 +
-                model.relation_embedding.norm(p = 3).norm(p = 3)**3
+                    model.entity_embedding.norm(p=3) ** 3 +
+                    model.relation_embedding.norm(p=3).norm(p=3) ** 3
             )
             loss = loss + regularization
             regularization_log = {'regularization': regularization.item()}
