6c7e85359d19f52a959fd3ec6810413ee92cbe14
guixiangyu <guixiangyu1@gmail.com>
2020-07-20 15:40:55 +0800

    dir_ampone

--- codes/model.py
+++ codes/model.py
@@ -579,7 +579,7 @@ class KGEModel(nn.Module):
 
         intensity = 2 * torch.cos(head_phase + relation - tail_phase) + 2.0
         # print('(rel_head * rel_tail).sum(): ', (rel_head * rel_tail).sum())
-        intensity = torch.abs((rel_head * rel_tail).sum(dim=2)) * intensity
+        intensity = torch.abs((rel_head * rel_tail).sum(dim=2, keepdims=True)) * intensity
 
         score = intensity.sum(dim=2) * 0.005 - self.gamma.item()
         return score
