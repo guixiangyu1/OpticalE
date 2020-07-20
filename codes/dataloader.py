519424131ab26ee2fa14c1e723d6423cadba822d
guixiangyu <guixiangyu1@gmail.com>
2020-07-20 09:53:54 +0800

    dir_ampone

--- codes/dataloader.py
+++ codes/dataloader.py
@@ -196,3 +196,125 @@ class BidirectionalOneShotIterator(object):
         while True:
             for data in dataloader:
                 yield data
+
+
+class TrainDataset_rel(Dataset):
+    def __init__(self, triples, nentity, nrelation, negative_sample_size, mode):
+        self.len = len(triples)
+        self.triples = triples
+        self.triple_set = set(triples)
+        self.nentity = nentity
+        self.nrelation = nrelation
+        self.negative_sample_size = negative_sample_size
+        self.mode = mode
+        self.count = self.count_frequency(triples)
+        # true_tail用来记录(h, r) 对应的正确的 t ， 属于diction, tail 记录于 np.array
+        self.true_head, self.true_tail, self.true_relation = self.get_true_head_and_tail(self.triples)
+
+    def __len__(self):
+        return self.len
+
+    # __getitem__ 就是让实例能跟字典一样取元素: P[idx]
+    def __getitem__(self, idx):
+        positive_sample = self.triples[idx]
+
+        head, relation, tail = positive_sample
+
+        # 将常出现的组合，权重反而降低；稀有的组合，反而权重更高。这种模式是模仿word2vec负采样中的 subsampling weight。
+        subsampling_weight = self.count[(head, relation)] + self.count[(tail, -relation - 1)]
+        subsampling_weight = torch.sqrt(1 / torch.Tensor([subsampling_weight]))
+
+        negative_sample_list = []
+        negative_sample_size = 0
+
+        while negative_sample_size < self.negative_sample_size:
+            negative_sample = np.random.randint(self.nrelation, size=int(self.negative_sample_size))
+
+            mask = np.in1d(
+                negative_sample,
+                self.true_relation[(head, tail)],
+                assume_unique=True,
+                invert=True
+            )
+            # numpy.array 的性质，A[[True, False]] 输出True对应的元素
+            negative_sample = negative_sample[mask]
+            negative_sample_list.append(negative_sample)
+            negative_sample_size += negative_sample.size
+
+        # np.concatenate() 将negtive_sample_list中的negtive sample array 进行拼接，得到一个完整的array后再裁剪
+        negative_sample = np.concatenate(negative_sample_list)[:self.negative_sample_size]
+
+        negative_sample = torch.from_numpy(negative_sample)
+
+        positive_sample = torch.LongTensor(positive_sample)
+
+        # 我加的，因为torch.index_select()中必须要longTensor
+        negative_sample = negative_sample.long()
+
+        return positive_sample, negative_sample, subsampling_weight, self.mode
+
+    @staticmethod
+    def collate_fn(data):
+        # merges a list of samples to form a mini-batch
+        positive_sample = torch.stack([_[0] for _ in data], dim=0)
+        negative_sample = torch.stack([_[1] for _ in data], dim=0)
+        subsample_weight = torch.cat([_[2] for _ in data], dim=0)
+        # 一个batch中，只有一个mode，因此只取其一
+        mode = data[0][3]
+        return positive_sample, negative_sample, subsample_weight, mode
+
+    @staticmethod
+    def count_frequency(triples, start=4):
+        '''
+        Get frequency of a partial triple like (head, relation) or (relation, tail)
+        The frequency will be used for subsampling like word2vec
+        '''
+        count = {}
+        for head, relation, tail in triples:
+            if (head, relation) not in count:
+                count[(head, relation)] = start
+            else:
+                count[(head, relation)] += 1
+
+            if (tail, -relation - 1) not in count:
+                count[(tail, -relation - 1)] = start
+            else:
+                count[(tail, -relation - 1)] += 1
+
+            if (head, tail) not in count:
+                count[(head, tail)] = start
+            else:
+                count[(head, tail)] += 1
+        return count
+
+    @staticmethod
+    def get_true_head_and_tail(triples):
+        '''
+        Build a dictionary of true triples that will
+        be used to filter these true triples for negative sampling
+        '''
+
+        true_head = {}
+        true_tail = {}
+        true_relation = {}
+
+        for head, relation, tail in triples:
+            if (head, relation) not in true_tail:
+                true_tail[(head, relation)] = []
+            true_tail[(head, relation)].append(tail)
+            if (relation, tail) not in true_head:
+                true_head[(relation, tail)] = []
+            true_head[(relation, tail)].append(head)
+            if (head, tail) not in true_relation:
+                true_relation[(head, tail)] = []
+            true_relation[(head, tail)].append(relation)
+
+
+        for relation, tail in true_head:
+            true_head[(relation, tail)] = np.array(list(set(true_head[(relation, tail)])))
+        for head, relation in true_tail:
+            true_tail[(head, relation)] = np.array(list(set(true_tail[(head, relation)])))
+        for head, tail in true_relation:
+            true_relation[(head, tail)] = np.array(list(set(true_relation[(head, tail)])))
+
+        return true_head, true_tail, true_relation
