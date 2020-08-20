023f6fe5d5b233fd9432525ced53f5254d66edf2
guixiangyu <guixiangyu1@gmail.com>
2020-08-20 22:11:22 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -561,7 +561,7 @@ class KGEModel(nn.Module):
 
         intensity = 2 * torch.abs(torch.cos(head_dir - tail_dir)) * torch.cos(head_phase + relation - tail_phase) + 2.0
 
-        score = self.gamma.item() - intensity.sum(dim=2) * 0.012
+        score = self.gamma.item() - intensity.sum(dim=2) * 0.03
 
         return score
 
@@ -944,10 +944,10 @@ class KGEModel(nn.Module):
         # positive_score = model(positive_sample)
         # positive_score = F.logsigmoid(positive_score).squeeze(dim = 1)
 
-        negative_score = model((positive_sample, negative_sample), mode=mode) - 4.0
-        positive_score = model(positive_sample) + 4.0
+        negative_score = model((positive_sample, negative_sample), mode=mode) - 2.0
+        positive_score = model(positive_sample) + 2.0
         # print(negative_score)
-        thre = 11.0 - 4.0 - 4.0
+        thre = 11.0 - 2.0 - 2.0
         negative_score1 = torch.where(negative_score > thre, -negative_score, negative_score)
 
         if args.negative_adversarial_sampling:
