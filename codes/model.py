56c832bd28dd08e7e3eaee3cd3534cb4f255ca8f
guixiangyu <guixiangyu1@gmail.com>
2020-08-21 20:39:27 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -964,8 +964,8 @@ class KGEModel(nn.Module):
         # positive_score = model(positive_sample)
         # positive_score = F.logsigmoid(positive_score).squeeze(dim = 1)
 
-        negative_score = model((positive_sample, negative_sample), mode=mode) - 4.0
-        positive_score = model(positive_sample) + 2.0
+        negative_score = model((positive_sample, negative_sample), mode=mode)
+        positive_score = model(positive_sample)
         # print(negative_score)
         thre = 13
         negative_score1 = torch.where(negative_score > thre, -negative_score, negative_score)
