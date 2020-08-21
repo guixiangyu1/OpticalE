7ac89bd3dbc78e933813b8ab5df5c2299f665b82
guixiangyu <guixiangyu1@gmail.com>
2020-08-21 17:28:50 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -66,7 +66,7 @@ class KGEModel(nn.Module):
         )
         
         if model_name == 'pRotatE' or model_name == 'rOpticalE_mult' or model_name == 'OpticalE_symmetric' or \
-                model_name == 'OpticaE_dir_ampone' or model_name=='OpticalE_interference_term':
+                model_name == 'OpticalE_dir_ampone' or model_name=='OpticalE_interference_term':
             # self.modulus = nn.Parameter(torch.Tensor([[0.5 * self.embedding_range.item()]]))
             self.modulus = nn.Parameter(torch.Tensor([[self.gamma.item() * 0.5 / self.hidden_dim]]))
         
