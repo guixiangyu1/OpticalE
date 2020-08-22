13638efe002a6ff39d4c483d486eee954416a774
guixiangyu <guixiangyu1@gmail.com>
2020-08-22 19:32:03 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -964,7 +964,7 @@ class KGEModel(nn.Module):
         # positive_score = model(positive_sample)
         # positive_score = F.logsigmoid(positive_score).squeeze(dim = 1)
 
-        negative_score = model((positive_sample, negative_sample), mode=mode) - 1.0
+        negative_score = model((positive_sample, negative_sample), mode=mode)
         positive_score = model(positive_sample) + 1.0
         # print(negative_score)
         thre = 4.0 - 1.0 - 1.0
