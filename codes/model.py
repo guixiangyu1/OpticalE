b32f9bdc57792b024e2f2785a124879b4ff7ad74
guixiangyu <guixiangyu1@gmail.com>
2020-08-23 19:07:05 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -579,7 +579,7 @@ class KGEModel(nn.Module):
         tail_phase = tail_phase / (self.embedding_range.item() / pi)
         rel_phase = rel_phase / (self.embedding_range.item() / pi)
 
-        hr_mod = torch.abs(head_mod) + rel_mod
+        hr_mod = torch.abs(head_mod * rel_mod)
         I = hr_mod ** 2 + tail_mod ** 2 + 2 * (hr_mod * tail_mod).abs() * torch.cos(head_phase + rel_phase - tail_phase)
         score = self.gamma.item() - I.sum(dim=2)
         return score
