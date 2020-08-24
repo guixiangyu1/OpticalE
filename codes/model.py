9e8e662bb8af3f45767a7460b84359c508deedde
guixiangyu <guixiangyu1@gmail.com>
2020-08-24 12:02:04 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -79,7 +79,7 @@ class KGEModel(nn.Module):
                               'OpticalE_amp', 'OpticalE_dir', 'pOpticalE_dir', 'OpticalE_2unit', 'rOpticalE_2unit',\
                               'OpticalE_onedir', 'OpticalE_weight', 'OpticalE_mult', 'rOpticalE_mult', 'functan',\
                               'Rotate_double', 'Rotate_double_test', 'OpticalE_symmetric', 'OpticalE_polarization', 'OpticalE_dir_ampone', 'OpticalE_relevant_ampone',\
-                              'OpticalE_intefere', 'OpticalE_interference_term', 'HopticalE']:
+                              'OpticalE_intefere', 'OpticalE_interference_term', 'HopticalE_re']:
             raise ValueError('model %s not supported' % model_name)
             
         if model_name == 'RotatE' and (not double_entity_embedding or double_relation_embedding):
@@ -198,7 +198,7 @@ class KGEModel(nn.Module):
             'OpticalE_relevant_ampone': self.OpticalE_relevant_ampone,
             'OpticalE_intefere': self.OpticalE_intefere,
             'OpticalE_interference_term': self.OpticalE_interference_term,
-            'HopticalE': self.HopticalE
+            'HopticalE_re': self.HopticalE_re
         }
         
         if self.model_name in model_func:
