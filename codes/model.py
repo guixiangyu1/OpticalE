780e576d85435790897bc2e054a2faf8732bf199
guixiangyu <guixiangyu1@gmail.com>
2020-08-28 11:55:47 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1218,7 +1218,7 @@ class KGEModel(nn.Module):
         negative_score = model((positive_sample, negative_sample), mode=mode)
         positive_score = model(positive_sample)
         # print(negative_score)
-        thre = 900
+        thre = 9
         negative_score1 = torch.where(negative_score > thre, -negative_score, negative_score)
 
         if args.negative_adversarial_sampling:
