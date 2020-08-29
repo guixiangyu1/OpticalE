5c2302ef68aa8a399d3eaf8168b6544ab302e421
guixiangyu <guixiangyu1@gmail.com>
2020-08-29 19:01:19 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -111,7 +111,8 @@ class KGEModel(nn.Module):
                               'OpticalE_onedir', 'OpticalE_weight', 'OpticalE_mult', 'rOpticalE_mult', 'functan',\
                               'Rotate_double', 'Rotate_double_test', 'OpticalE_symmetric', 'OpticalE_polarization', 'OpticalE_dir_ampone', 'OpticalE_relevant_ampone',\
                               'OpticalE_intefere', 'OpticalE_interference_term', 'HopticalE', 'HopticalE_re', 'regOpticalE', 'regOpticalE_r', 'HAKE', 'HAKE_one', \
-                              'HopticalE_one', 'OpticalE_matrix', 'TransE_gamma', 'TransE_weight', 'Projection', 'ProjectionH', 'ProjectionT', 'ProjectionHT', 'ModE']:
+                              'HopticalE_one', 'OpticalE_matrix', 'TransE_gamma', 'TransE_weight', 'Projection', 'ProjectionH', 'ProjectionT', 'ProjectionHT', \
+                              'ModE', 'ProtatE_head']:
             raise ValueError('model %s not supported' % model_name)
             
         if model_name == 'RotatE' and (not double_entity_embedding or double_relation_embedding):
@@ -245,7 +246,8 @@ class KGEModel(nn.Module):
             'ProjectionH': self.ProjectionH,
             'ProjectionT': self.ProjectionT,
             'ProjectionHT': self.ProjectionHT,
-            'ModE': self.ModE
+            'ModE': self.ModE,
+            'ProtatE_head': self.ProtatE_head
 
         }
         
