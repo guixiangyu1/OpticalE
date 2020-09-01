e5e56377a381cf2ced8da742a3396e697ef75045
guixiangyu <guixiangyu1@gmail.com>
2020-09-01 14:41:59 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -60,6 +60,8 @@ class KGEModel(nn.Module):
             self.relation_dim = self.relation_dim + 1
         if model_name=='adapTransE':
             self.relation_dim = self.relation_dim + 1
+        if model_name=='CylinderE':
+            self.entity_dim = hidden_dim * 3 if double_entity_embedding else hidden_dim
         
         self.entity_embedding = nn.Parameter(torch.zeros(nentity, self.entity_dim))
         nn.init.uniform_(
