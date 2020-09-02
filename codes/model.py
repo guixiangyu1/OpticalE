b7997d90e4224ad8e77524e9d8e93c0c4c7bd0e7
guixiangyu <guixiangyu1@gmail.com>
2020-09-02 14:20:38 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -28,8 +28,6 @@ class KGEModel(nn.Module):
         self.hidden_dim = hidden_dim
         self.epsilon = 2.0
 
-        self.m_weight = nn.Parameter(torch.Tensor([[1.0]]))
-
         # gamma 的default是12.0
         self.gamma = nn.Parameter(
             torch.Tensor([gamma]), 
@@ -438,8 +436,8 @@ class KGEModel(nn.Module):
         tail_phase = t_p / (self.embedding_range.item() / pi)
         rel_phase = r_p / (self.embedding_range.item() / pi)
 
-
-        dis_m = ((h_z.abs() + r_z).abs() - t_z.abs()).norm(p=2, dim=2) * self.mweight
+        self.m_weight = nn.Parameter(torch.Tensor([[1.0]]))
+        dis_m = ((h_z.abs() + r_z).abs() - t_z.abs()).norm(p=2, dim=2) * self.m_weight
         score_m = - dis_m
         p_m = torch.sigmoid(score_m)
         # print(p_m)
