a59f862115fa507d2962bb06542d9505b521c743
guixiangyu <guixiangyu1@gmail.com>
2020-09-02 16:40:15 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -63,8 +63,8 @@ class KGEModel(nn.Module):
         if model_name == 'HTR':
             self.entity_dim = hidden_dim * 4 if double_entity_embedding else hidden_dim
             self.relation_dim = hidden_dim * 4 if double_relation_embedding else hidden_dim
-        if model_name=='CylinderE':
-            self.entity_dim = hidden_dim * 3 if double_entity_embedding else hidden_dim
+        # if model_name=='CylinderE':
+        #     self.entity_dim = hidden_dim * 3 if double_entity_embedding else hidden_dim
             # self.relation_dim = hidden_dim * 3 if double_relation_embedding else hidden_dim
         
         self.entity_embedding = nn.Parameter(torch.zeros(nentity, self.entity_dim))
