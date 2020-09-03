1711d561b732ae6a7ec7506c4298a54ba8deb565
guixiangyu <guixiangyu1@gmail.com>
2020-09-03 09:09:28 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -519,7 +519,7 @@ class KGEModel(nn.Module):
         else:
             phase = head_phase + rel_phase - tail_phase
         p_score = torch.norm(torch.abs(torch.sin(phase / 2)), p=1, dim=2)
-        score = m_score * self.m_weight + p_score * self.modulus * 0.5
+        score = m_score * self.m_weight + p_score * self.modulus
 
         return self.gamma.item() - score
 
