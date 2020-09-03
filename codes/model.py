d57ceb97c6d84a9432a84efa23f9d83d32442a01
guixiangyu <guixiangyu1@gmail.com>
2020-09-03 09:31:33 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -386,12 +386,12 @@ class KGEModel(nn.Module):
         tail1, tail2 = torch.chunk(tail, 2, dim=2)
         relation1, relation2 = torch.chunk(relation, 2, dim=2)
         if mode=='head-batch':
-            phase1 = head1 + (relation1 - tail1)
+            phase1 = (head1 + (relation1 - tail1)) * 10
             phase2 = head2 + (relation2 - tail2)
         else:
-            phase1 = head1 + relation1 - tail1
+            phase1 = (head1 + relation1 - tail1) * 10
             phase2 = head2 + relation2 - tail2
-        score1 = torch.norm(torch.sin(phase1 / 2), p=2, dim=2) * self.modulus * 5
+        score1 = torch.norm(torch.sin(phase1 / 2), p=2, dim=2) * self.modulus
         score2 = torch.sum(torch.abs(torch.sin(phase2 / 2)), dim=2) * self.modulus
         print(score1.mean())
         score = self.gamma.item() - (score1 + score2)
