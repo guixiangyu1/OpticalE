7ad6702b4537689092c2d1012b059227bdd066be
guixiangyu <guixiangyu1@gmail.com>
2020-09-09 10:09:21 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -524,8 +524,8 @@ class KGEModel(nn.Module):
 
         rel1, rel2 = torch.chunk(relation, 2, dim=2)
 
-        k_hr = k_h.abs() + k_t.abs()
-        phase = head_phase + k_hr * rel1 - tail_phase
+        k_hr = k_h.abs() - k_t.abs()
+        phase = head_phase + k_hr * rel1 + rel2 - tail_phase
         score = torch.sum(torch.abs(torch.sin(phase / 2)), dim=2)
         score = self.gamma.item() - score * self.modulus
         return score
