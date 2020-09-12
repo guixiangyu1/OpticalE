a6e784f28713689c664d1d723bf8deb1d590955d
guixiangyu <guixiangyu1@gmail.com>
2020-09-12 21:48:35 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -496,54 +496,54 @@ class KGEModel(nn.Module):
         # return score
     ###############################################################
         # HAKE + cylinder
-        pi = 3.14159262358979323846
+        #pi = 3.14159262358979323846
 
+        #head1, head2 = torch.chunk(head, 2, dim=2)
+        #tail1, tail2 = torch.chunk(tail, 2, dim=2)
+        #rel1, rel2 = torch.chunk(relation, 2, dim=2)
+        #
+        #rel2 = rel2 / (self.embedding_range.item() / pi)
+        #head2 = head2 / (self.embedding_range.item() / pi)
+        #tail2 = tail2 / (self.embedding_range.item() / pi)
+        #
+        #phase = head2 + rel2 - tail2
+        #
+        #score1 = torch.norm((head1 * rel1.abs() - tail1), p=2, dim=2) * self.m_weight
+        # print(score1.mean())
+        #radium = (1.0 - score1/30).detach()
+        #score2 = torch.sum(torch.abs(torch.sin(phase / 2)), dim=2) * self.modulus
+        #score = self.gamma.item() - (score1 + score2)
+        #return score
+
+    ###############################################################
+        #hake + rotate
+        pi = 3.14159262358979323846
+        #
         head1, head2 = torch.chunk(head, 2, dim=2)
         tail1, tail2 = torch.chunk(tail, 2, dim=2)
         rel1, rel2 = torch.chunk(relation, 2, dim=2)
+        #
+        # head1 = head1.abs()
+        # tail1 = tail1.abs()
+        rel1 = rel1.abs()
         #
         rel2 = rel2 / (self.embedding_range.item() / pi)
         head2 = head2 / (self.embedding_range.item() / pi)
         tail2 = tail2 / (self.embedding_range.item() / pi)
         #
-        phase = head2 + rel2 - tail2
+        hr_p = head2 + rel2
+        hr_m = head1 * rel1
         #
-        score1 = torch.norm((head1 * rel1.abs() - tail1), p=2, dim=2) * self.m_weight
-        # print(score1.mean())
-        #radium = (1.0 - score1/30).detach()
-        score2 = torch.sum(torch.abs(torch.sin(phase / 2)), dim=2) * self.modulus
-        score = self.gamma.item() - (score1 + score2)
+        score1 = torch.norm((hr_m - tail1), p=2, dim=2) * self.m_weight
+        #
+        x = hr_m.detach() * torch.cos(hr_p) - tail1.detach() * torch.cos(tail2)
+        y = hr_m.detach() * torch.sin(hr_p) - tail1.detach() * torch.sin(tail2)
+        xy = torch.stack([x,y], dim=0)
+        score2 = torch.norm(xy, dim=0)
+        #
+        print(score1.mean())
+        score = self.gamma.item() - (score1 + score2.sum(dim=2))
         return score
-
-    ###############################################################
-        #hake + rotate
-        # pi = 3.14159262358979323846
-        #
-        # head1, head2 = torch.chunk(head, 2, dim=2)
-        # tail1, tail2 = torch.chunk(tail, 2, dim=2)
-        # rel1, rel2 = torch.chunk(relation, 2, dim=2)
-        #
-        # head1 = head1.abs()
-        # tail1 = tail1.abs()
-        # rel1 = rel1.abs()
-        #
-        # rel2 = rel2 / (self.embedding_range.item() / pi)
-        # head2 = head2 / (self.embedding_range.item() / pi)
-        # tail2 = tail2 / (self.embedding_range.item() / pi)
-        #
-        # hr_p = head2 + rel2
-        # hr_m = head1 * rel1
-        #
-        # score1 = torch.norm((hr_m - tail1), p=1, dim=2) * self.m_weight
-        #
-        # x = hr_m.detach() * torch.cos(hr_p) - tail1.detach() * torch.cos(tail2)
-        # y = hr_m.detach() * torch.sin(hr_p) - tail1.detach() * torch.sin(tail2)
-        # xy = torch.stack([x,y], dim=0)
-        # score2 = torch.norm(xy, dim=0)
-        #
-        # print(score1.mean())
-        # score = self.gamma.item() - (score1 + score2.sum(dim=2))
-        # return score
     ##############################################################################################
 
 
