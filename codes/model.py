ae02a16380ef3aaf9874c4d48282194b8117f991
guixiangyu <guixiangyu1@gmail.com>
2020-09-14 10:53:36 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1455,7 +1455,7 @@ class KGEModel(nn.Module):
         # score = (tail_mod ** 2 + head_mod ** 2 + rel_mod ** 2) + 2 * (head_mod * rel_mod -  head_mod * tail_mod - rel_mod * tail_mod) \
         #        + self.modulus * torch.cos(head_phase + rel_phase - tail_phase).abs()
         score1 = (head_mod * rel_mod.abs() - tail_mod).norm(p=2, dim=2) * self.m_weight
-        score2 = torch.norm(torch.sin((head_phase + rel_phase - tail_phase)/2), p=2, dim=2) * self.modulus
+        score2 = torch.norm(torch.sin((head_phase + rel_phase - tail_phase)/2), p=1, dim=2) * self.modulus
 
         print(score1.mean())
         score = self.gamma.item() - (score1 + score2)
