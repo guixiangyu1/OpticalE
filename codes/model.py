20ce8141151a404d406a6f5055a48fa9ebc29581
guixiangyu <guixiangyu1@gmail.com>
2020-09-14 17:29:25 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -195,13 +195,13 @@ class KGEModel(nn.Module):
         if model_name == 'TestE1':
             nn.init.uniform_(
                 tensor=self.relation_embedding[:, :self.hidden_dim],
-                a=-0.5,
-                b=0.5
+                a=-0.1,
+                b=0.1
             )
             nn.init.uniform_(
                 tensor=self.entity_embedding[:, :self.hidden_dim],
-                a=-0.5,
-                b=0.5
+                a=-0.1,
+                b=0.1
             )
 
         # if model_name=='loopE':
