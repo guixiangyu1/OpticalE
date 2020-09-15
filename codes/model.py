6bd7216abfee538ee511ae18c1f736d9064084d7
guixiangyu <guixiangyu1@gmail.com>
2020-09-15 15:19:12 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -193,11 +193,11 @@ class KGEModel(nn.Module):
                 val=1.0
             )
 
-        # if model_name == 'TestE1':
-        #     nn.init.constant_(
-        #         tensor=self.relation_embedding[:, :self.hidden_dim],
-        #         val=1.0
-        #     )
+        if model_name == 'TestE1':
+            nn.init.constant_(
+                tensor=self.relation_embedding[:, :self.hidden_dim],
+                val=1.0
+            )
         #     nn.init.uniform_(
         #         tensor=self.entity_embedding[:, :self.hidden_dim],
         #         a=-0.3,
