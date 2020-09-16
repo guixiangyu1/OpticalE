d4c49189b9715d75b7c5ff61d7c294daaf0b6e6e
guixiangyu <guixiangyu1@gmail.com>
2020-09-16 16:30:33 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -475,9 +475,9 @@ class KGEModel(nn.Module):
         tail2 = tail2 / (self.embedding_range.item() / pi)
         #
         # I = 2 * head1 * tail1 * (1 + torch.cos(head2 + rel2 - tail2)) / (head1 ** 2 + tail1 ** 2)
-        I = torch.norm(torch.sin((head2 + rel2 - tail2)/2), p=1, dim=2)
-
-        score = self.gamma.item() - I * self.modulus
+        # I = torch.norm(torch.sin((head2 + rel2 - tail2)/2), p=1, dim=2)
+        I = torch.sin(head2 + rel2 - tail2)
+        score = self.gamma.item() - I * 0.015
 
         return score
 
