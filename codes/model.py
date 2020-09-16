87abb17deb76b5892fb99bab438079ad563c59d3
guixiangyu <guixiangyu1@gmail.com>
2020-09-16 08:37:53 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -68,8 +68,8 @@ class KGEModel(nn.Module):
             self.relation_dim = hidden_dim * 3 if double_relation_embedding else hidden_dim
         if model_name=='loopE':
             self.relation_dim = self.relation_dim + 1
-        if model_name=='TestE':
-            self.entity_dim = hidden_dim * 3 if double_entity_embedding else hidden_dim
+        # if model_name=='TestE':
+        #     self.entity_dim = hidden_dim * 3 if double_entity_embedding else hidden_dim
         # if model_name=='TestE1':
         #     self.relation_dim = self.relation_dim + 1
 
