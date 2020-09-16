1d98f9d948c959384bbcd121ee727a84d92951ba
guixiangyu <guixiangyu1@gmail.com>
2020-09-16 21:48:43 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -194,7 +194,10 @@ class KGEModel(nn.Module):
                 val=1.0
             )
 
-
+        nn.init.constant_(
+            tensor=self.relation_embedding[:, :self.hidden_dim],
+            val=1.0
+        )
 
         if model_name == 'TestE1':
             nn.init.constant_(
