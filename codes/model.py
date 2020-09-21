98899b635ce71f38d5306784917c27683015f152
guixiangyu <guixiangyu1@gmail.com>
2020-09-21 16:00:43 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -42,7 +42,7 @@ class KGEModel(nn.Module):
         #              requires_grad=False
         #          )
         self.embedding_range = nn.Parameter(
-            torch.Tensor([(self.gamma.item() + self.epsilon) / hidden_dim]),
+            torch.Tensor([0.11]),
             requires_grad=False
         )
         # self.embedding_range = nn.Parameter(torch.Tensor([3.14]))
@@ -199,11 +199,11 @@ class KGEModel(nn.Module):
                 val=1.0
             )
 
-            nn.init.uniform_(
-                tensor=self.entity_embedding[:, :self.hidden_dim],
-                a=0.0,
-                b=0.11
-            )
+            # nn.init.uniform_(
+            #     tensor=self.entity_embedding[:, :self.hidden_dim],
+            #     a=0.0,
+            #     b=0.11
+            # )
 
         if model_name == 'TestE1':
             nn.init.constant_(
