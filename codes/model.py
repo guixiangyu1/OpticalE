c9edcecb630c2516a2be3086b5a4171073e57540
guixiangyu <guixiangyu1@gmail.com>
2020-09-23 09:18:13 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -490,8 +490,8 @@ class KGEModel(nn.Module):
 
         phase = head2 + rel2 - tail2
 
-        head1 = F.normalize(head1.abs(), p=2, dim=2) * self.gamma.item()
-        tail1 = F.normalize(tail1.abs(), p=2, dim=2) * self.gamma.item()
+        head1 = F.normalize(head1.abs(), p=2, dim=2) * self.gamma.item() * 0.5
+        tail1 = F.normalize(tail1.abs(), p=2, dim=2) * self.gamma.item() * 0.5
 
         I = head1 ** 2 + tail1 ** 2 + 2 * head1 * tail1 * torch.cos(phase)
         score = self.gamma.item() - I.sum(dim=2)
