4fe6082cd64b0c0716c64c4430e9965859dd95d0
guixiangyu <guixiangyu1@gmail.com>
2020-09-24 15:48:38 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -206,7 +206,7 @@ class KGEModel(nn.Module):
             nn.init.uniform_(
                 tensor=self.entity_embedding[:, :self.hidden_dim],
                 a=0.0,
-                b=1.0
+                b=0.1
             )
             # nn.init.uniform_(
             #     tensor=self.relation_embedding[:, :self.hidden_dim],
@@ -537,7 +537,7 @@ class KGEModel(nn.Module):
         tail2 = tail2 / (self.embedding_range.item() / pi)
 
 
-        theta = 1
+        theta = 0.1
         head1 = head1.abs() % theta
         tail1 = tail1.abs() % theta
 
