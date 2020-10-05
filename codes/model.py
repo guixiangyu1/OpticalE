cffe4d860d78d147cea6de218c728445352fbbe7
guixiangyu <guixiangyu1@gmail.com>
2020-10-05 10:00:15 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1603,7 +1603,7 @@ class KGEModel(nn.Module):
 
 
 
-        score = self.gamma.item() - intensity * 3.5
+        score = self.gamma.item() - intensity * 4.0
 
         return score
 
@@ -2232,16 +2232,16 @@ class KGEModel(nn.Module):
         negative_score = model((positive_sample, negative_sample), mode=mode)
         positive_score = model(positive_sample)
         # print(negative_score)
-        # thre = 2
-        # negative_score1 = torch.where(negative_score > thre, -negative_score, negative_score)
+        thre = 2
+        negative_score1 = torch.where(negative_score > thre, -negative_score, negative_score)
         if args.negative_adversarial_sampling:
             # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
             # detach() 函数起到了阻断backpropogation的作用
-            negative_score = (F.softmax(negative_score * args.adversarial_temperature, dim=1).detach()
-                              * F.logsigmoid(- negative_score)).sum(dim=1)
+            negative_score = (F.softmax(negative_score1 * args.adversarial_temperature, dim=1).detach()
+                              * F.logsigmoid(- negative_score1)).sum(dim=1)
 
         else:
-            negative_score = F.logsigmoid(- negative_score).mean(dim=1)
+            negative_score = F.logsigmoid(- negative_score1).mean(dim=1)
 
         # mode = 'single'
         # positive_score = positive_score.squeeze(dim=1)
