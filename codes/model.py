cf3e60b8423417270037874dafda10fb0dfe6046
guixiangyu <guixiangyu1@gmail.com>
2020-10-06 10:22:30 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1596,14 +1596,12 @@ class KGEModel(nn.Module):
         # intensity = 2 * torch.abs(torch.cos(head_dir - tail_dir)) * torch.cos(head_phase + relation - tail_phase) + 2
         inferece = torch.abs(torch.cos(head_dir - tail_dir))
         intensity = 2 * inferece * torch.cos(head_phase + relation - tail_phase) + 2
-        score2 = (1 - inferece).norm(p=2, dim=2) * 0.05
-        print(score2.mean())
         # var = torch.var(intensity, dim=2)
         # print(var.mean())
 
         # intensity = (F.softmax(intensity * 0.1, dim=2).detach() * intensity).sum(dim=2)
 
-        score = self.gamma.item() - intensity.sum(dim=2) * self.modulus - score2
+        score = self.gamma.item() - intensity.sum(dim=2) * 5.0
 
         return score
 
