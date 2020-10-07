906ddc2e1188ecc5bd3fcbaf1beb3a503de1276a
guixiangyu <guixiangyu1@gmail.com>
2020-10-07 15:51:14 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -29,7 +29,7 @@ class KGEModel(nn.Module):
         self.epsilon = 2.0
         self.m_weight = nn.Parameter(torch.Tensor([[5.5]]))
         self.p_weight = nn.Parameter(torch.Tensor([[0.1]]))
-        self.bia = nn.Parameter(torch.Tensor([[0.0]]))
+        self.bia = nn.Parameter(torch.Tensor([[0.1]]))
         # gamma 的default是12.0
         self.gamma = nn.Parameter(
             torch.Tensor([gamma]), 
@@ -43,7 +43,7 @@ class KGEModel(nn.Module):
                      requires_grad=False
                  )
         self.dir_range = nn.Parameter(
-            torch.Tensor([self.embedding_range.item() * 10]),
+            torch.Tensor([self.embedding_range.item()]),
             requires_grad=False
         )
         # self.embedding_range = nn.Parameter(
@@ -1609,7 +1609,7 @@ class KGEModel(nn.Module):
         # inferece = torch.abs(torch.cos(head_dir - tail_dir + bia))
 
         # intensity = 2 * torch.abs(torch.cos(head_dir - tail_dir)) * torch.cos(head_phase + relation - tail_phase) + 2
-        inferece = torch.abs(torch.cos(head_dir - tail_dir +  0.1))
+        inferece = torch.abs(torch.cos(head_dir - tail_dir +  self.bia))
         intensity = 2 * inferece * torch.cos(head_phase + relation - tail_phase) + 2
         # var = torch.var(intensity, dim=2)
         # print(var.mean())
