f06cb6b6d7eb28b6c8a7f319260a48f4f29f1514
guixiangyu <guixiangyu1@gmail.com>
2020-10-08 17:12:29 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -2291,8 +2291,8 @@ class KGEModel(nn.Module):
         negative_score = model((positive_sample, negative_sample), mode=mode)
         positive_score = model(positive_sample)
         # print(negative_score)
-        thre = 1.0
-        negative_score1 = torch.where(negative_score > thre, negative_score * 0.1, negative_score)
+        thre = 4.0
+        negative_score1 = torch.where(negative_score > thre, -negative_score, negative_score)
         if args.negative_adversarial_sampling:
             # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
             # detach() 函数起到了阻断backpropogation的作用
