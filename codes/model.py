808ff6c149b938f835a6f2b36041a8c67d5e3997
guixiangyu <guixiangyu1@gmail.com>
2020-10-09 14:55:17 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -518,8 +518,8 @@ class KGEModel(nn.Module):
 
         # head1 = head1 / (self.dir_range.item() / pi)
         # tail1 = tail1 / (self.dir_range.item() / pi)
-        head1 = head1 * 3
-        tail1 = tail1 * 3
+        head1 = head1 * 30
+        tail1 = tail1 * 30
 
         rel2 = relation / (self.embedding_range.item() / pi)
         head2 = head2 / (self.embedding_range.item() / pi)
