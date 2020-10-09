4b446383c8ba30b0d3b3062bd14f1d2bd04314ac
guixiangyu <guixiangyu1@gmail.com>
2020-10-09 18:08:28 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -526,6 +526,7 @@ class KGEModel(nn.Module):
         tail2 = tail2 / (self.embedding_range.item() / pi)
 
         infere = torch.sigmoid(2 - (head1 - tail1).abs())
+        print(head1)
         print(infere.mean())
         intensity = 2 + 2 * torch.cos(head2 + rel2 - tail2) * infere
 
