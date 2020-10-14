a0b1011c501c9931fded93376a5517e1bba64591
guixiangyu <guixiangyu1@gmail.com>
2020-10-14 15:12:57 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1659,10 +1659,10 @@ class KGEModel(nn.Module):
         # head_dir = head_dir / (self.dir_range.item() / pi)
         # tail_dir = tail_dir / (self.dir_range.item() / pi)
 
-        head_dir = head_dir / (self.dir_range.item())
-        tail_dir = tail_dir / (self.dir_range.item())
-        # head_dir = head_dir * 100
-        # tail_dir = tail_dir * 100
+        # head_dir = head_dir / (self.dir_range.item())
+        # tail_dir = tail_dir / (self.dir_range.item())
+        head_dir = head_dir * 200
+        tail_dir = tail_dir * 200
 
         # inference = torch.abs(torch.sin(head_dir - tail_dir + 0.001))
         inference = torch.exp(-2 * (head_dir - tail_dir) ** 2)
