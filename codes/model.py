eb3b7cb80b5701b1a0bd11c0074144df3dda054f
guixiangyu <guixiangyu1@gmail.com>
2020-10-14 08:46:43 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1668,7 +1668,7 @@ class KGEModel(nn.Module):
         # print(coefficient_list)
 
 
-        score = self.gamma.item() - intensity.sum(dim=2) * 0.005
+        score = self.gamma.item() - intensity.sum(dim=2) * 0.006
         # print(self.bia)
 
         return score
