a8c27c33b8bb90ce858241fc146271ac96c928df
guixiangyu <guixiangyu1@gmail.com>
2020-10-15 22:22:33 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1663,7 +1663,7 @@ class KGEModel(nn.Module):
 
         inference = torch.abs(torch.cos(head_dir - tail_dir + 0.1))
         # inference = torch.exp(-(head_dir - tail_dir).abs() * 2)
-        intensity = -2 * inference * torch.cos(head_phase + relation - tail_phase) + 2.0
+        intensity = 2 * inference * torch.cos(head_phase + relation - tail_phase) + 2.0
 
 
         score = self.gamma.item() - intensity.sum(dim=2) * 0.008
