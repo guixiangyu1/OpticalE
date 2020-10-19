2498244c10bbaa8b113b4a156451e3735036346d
guixiangyu <guixiangyu1@gmail.com>
2020-10-19 14:30:37 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -2325,7 +2325,7 @@ class KGEModel(nn.Module):
         else:
             positive_sample_loss = - (subsampling_weight * positive_score).sum()/subsampling_weight.sum()
             negative_sample_loss = - (subsampling_weight * negative_score).sum()/subsampling_weight.sum()
-        P_inference_loss = (torch.relu(0.8 - P_inference)).mean()
+        P_inference_loss = (torch.relu(0.8 - P_inference)).sum() * 0.01
         # N_inference_loss = (torch.relu(P_inference - 0.6)).mean()
 
 
