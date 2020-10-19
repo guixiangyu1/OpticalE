631d5753c7944183a2762acdd6f689070de1fcaa
guixiangyu <guixiangyu1@gmail.com>
2020-10-19 11:07:26 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -43,7 +43,7 @@ class KGEModel(nn.Module):
                      requires_grad=False
                  )
         self.dir_range = nn.Parameter(
-            torch.Tensor([self.embedding_range.item() * 20]),
+            torch.Tensor([self.embedding_range.item()]),
             requires_grad=False
         )
         # self.embedding_range = nn.Parameter(
