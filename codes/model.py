67216113ee89fa768a7a8cfda7247c9fa1b0f4e8
guixiangyu <guixiangyu1@gmail.com>
2020-10-19 17:06:09 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -2325,12 +2325,12 @@ class KGEModel(nn.Module):
         else:
             positive_sample_loss = - (subsampling_weight * positive_score).sum()/subsampling_weight.sum()
             negative_sample_loss = - (subsampling_weight * negative_score).sum()/subsampling_weight.sum()
-        # P_inference_loss = (torch.relu(0.8 - P_inference)).sum() * 0.01
+        P_inference_loss = (torch.relu(0.8 - P_inference)).sum() * 0.01
         # N_inference_loss = (torch.relu(P_inference - 0.6)).mean()
 
 
         loss = (positive_sample_loss + negative_sample_loss)/2
-        # loss = (P_inference_loss) + loss
+        loss = (P_inference_loss) + loss
 
         if args.regularization != 0.0:
             #Use L3 regularization for ComplEx and DistMult
