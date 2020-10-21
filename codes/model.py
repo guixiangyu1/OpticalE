01c85dd8828341ecf334e4d6a8966adf826afd10
guixiangyu <guixiangyu1@gmail.com>
2020-10-21 11:15:40 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1625,11 +1625,13 @@ class KGEModel(nn.Module):
         infR = infR / (self.embedding_range.item() / pi)
         infT = infT / (self.embedding_range.item() / pi)
 
-        score = 2 + 2 * torch.cos(infH + infR - infT)
-        inference = torch.sigmoid(self.gamma.item() - score.sum(dim=2, keepdim=True) * 0.008)
-        # inference = (2 + 2 * torch.cos(infH + infR - infT))/2
-        zeros = torch.zeros_like(inference)
-        inference = torch.where(inference > 0.7, inference, zeros+0.7)
+        # score = 2 + 2 * torch.cos(infH + infR - infT)
+        # inference = torch.sigmoid(self.gamma.item() - score.sum(dim=2, keepdim=True) * 0.008)
+
+        # zeros = torch.zeros_like(inference)
+        # inference = torch.where(inference > 0.7, inference, zeros+0.7)
+
+        inference = 0.5 - 0.5 * torch.cos(infH + infR - infT)
 
 
         head_phase = head / (self.embedding_range.item() / pi)
