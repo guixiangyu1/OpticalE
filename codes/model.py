2d6eef95629cf0b32766b6710807262cef607166
guixiangyu <guixiangyu1@gmail.com>
2020-10-22 09:44:02 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -204,10 +204,11 @@ class KGEModel(nn.Module):
             #     val=1.0
             # )
 
-            # nn.init.constant_(
-            #     tensor=self.relation_embedding[:, :self.hidden_dim],
-            #     val=1.0
-            # )
+            nn.init.uniform_(
+                tensor=self.entity_embedding[:, 2*self.hidden_dim:],
+                a=-0.0000001,
+                b=0.0000001
+            )
 
             nn.init.uniform_(
                 tensor=self.entity_embedding[:, :self.hidden_dim],
