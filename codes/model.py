ce9b93e70f1fbc64b563e28d38d081f1cc2f0917
guixiangyu <guixiangyu1@gmail.com>
2020-10-23 21:34:19 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1655,8 +1655,7 @@ class KGEModel(nn.Module):
         # inference = torch.exp(-(distance**2) * 10)
         intensity = 2 * inference * torch.cos((head_phase + rel_phase - tail_phase)) + 2
 
-
-        score = self.gamma.item() - intensity.sum(dim=2)
+        score = self.gamma.item() - intensity.sum(dim=2) * 0.005
         # print(inference.mean())
 
         # print(self.m_weight)
