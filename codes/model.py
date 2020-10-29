3891c96f65faba8d56508f748ae4f1d68cae7745
guixiangyu <guixiangyu1@gmail.com>
2020-10-29 11:11:45 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -74,8 +74,8 @@ class KGEModel(nn.Module):
             self.relation_dim = hidden_dim * 3 if double_relation_embedding else hidden_dim
         if model_name=='loopE':
             self.relation_dim = self.relation_dim + 1
-        if model_name=='TestE':
-            self.entity_dim = hidden_dim * 3 if double_entity_embedding else hidden_dim
+        # if model_name=='TestE':
+        #     self.entity_dim = hidden_dim * 3 if double_entity_embedding else hidden_dim
 
         self.entity_embedding = nn.Parameter(torch.zeros(nentity, self.entity_dim))
         nn.init.uniform_(
@@ -209,18 +209,18 @@ class KGEModel(nn.Module):
 
 
 
-        if model_name=='TestE':
+        # if model_name=='TestE':
 
             # nn.init.constant_(
             #     tensor=self.relation_embedding[:, 2*self.hidden_dim:],
             #     val=1.0
             # )
 
-            nn.init.uniform_(
-                tensor=self.entity_embedding[:, 2*self.hidden_dim:],
-                a=-0.00000001,
-                b= 0.00000001
-            )
+            # nn.init.uniform_(
+            #     tensor=self.entity_embedding[:, 2*self.hidden_dim:],
+            #     a=-0.00000001,
+            #     b= 0.00000001
+            # )
             #
             # nn.init.uniform_(
             #     tensor=self.entity_embedding[:, :self.hidden_dim],
@@ -228,10 +228,10 @@ class KGEModel(nn.Module):
             #     b=self.embedding_range.item() *10
             # )
 
-            nn.init.constant_(
-                tensor=self.entity_embedding[:, :self.hidden_dim],
-                val=1.7888 * 2
-            )
+            # nn.init.constant_(
+            #     tensor=self.entity_embedding[:, :self.hidden_dim],
+            #     val=1.7888 * 2
+            # )
             # nn.init.uniform_(
             #     tensor=self.relation_embedding[:, :self.hidden_dim],
             #     a=-2.0,
