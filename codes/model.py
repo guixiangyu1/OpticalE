48c8a0041b88f8fd0f72cd49d9f49a9a4130905d
guixiangyu <guixiangyu1@gmail.com>
2020-10-29 20:13:31 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1697,7 +1697,7 @@ class KGEModel(nn.Module):
         intensity = 2 * inference * torch.cos((head_phase + rel_phase - tail_phase)) + 2
         # intensity = 3.0 - torch.relu(3.0 - intensity)
         m = nn.Dropout(p=0.1)
-        intensity = m(intensity) * 0.9
+        intensity = m(intensity)
         # weight = torch.ones(self.hidden_dim).cuda()
         # weight = m(weight)
 
