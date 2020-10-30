3e3a7d47b4fbc008157ac82fa575fc0045a828fd
guixiangyu <guixiangyu1@gmail.com>
2020-10-30 10:24:14 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -251,17 +251,17 @@ class KGEModel(nn.Module):
             )
 
         if model_name=='OpticalE_dir_ampone':
-            nn.init.uniform_(
-                tensor=self.entity_embedding[:, :self.hidden_dim],
-                a=-self.dir_range.item(),
-                b=self.dir_range.item()
-            )
-
             # nn.init.uniform_(
-            #    tensor=self.entity_embedding,
-            #    a=-0.0000001,
-            #    b=0.0000001
+            #     tensor=self.entity_embedding[:, :self.hidden_dim],
+            #     a=-self.dir_range.item(),
+            #     b=self.dir_range.item()
             # )
+
+            nn.init.uniform_(
+               tensor=self.entity_embedding[:, :self.hidden_dim],
+               a=-0.0000001,
+               b=0.0000001
+            )
 
             # nn.init.constant_(
             #     tensor=self.entity_embedding[:, :self.hidden_dim],
