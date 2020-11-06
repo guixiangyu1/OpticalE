d09fb04ebe18e83bca5172ba1288d7cf92d04f1b
guixiangyu <guixiangyu1@gmail.com>
2020-11-06 10:34:45 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1682,8 +1682,8 @@ class KGEModel(nn.Module):
         head_dir, head_phase = torch.chunk(head, 2, dim=2)
         tail_dir, tail_phase = torch.chunk(tail, 2, dim=2)
 
-        tail_dir = tail_dir[:,:,:50]
-        head_dir = head_dir[:,:,:50]
+        tail_dir = tail_dir[:,:,:10]
+        head_dir = head_dir[:,:,:10]
 
 
         head_phase = head_phase / (self.embedding_range.item() / pi)
