81f946a6f1fe263f264a1e1cfefc2a31b87cae34
guixiangyu <guixiangyu1@gmail.com>
2020-11-06 17:28:25 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -250,18 +250,18 @@ class KGEModel(nn.Module):
                 val=1.0
             )
 
-        # if model_name=='OpticalE_dir_ampone':
+        if model_name=='OpticalE_dir_ampone':
             # nn.init.uniform_(
             #     tensor=self.entity_embedding[:, :self.hidden_dim],
             #     a=-self.dir_range.item(),
             #     b=self.dir_range.item()
             # )
 
-            # nn.init.uniform_(
-            #    tensor=self.entity_embedding[:, :2 * self.hidden_dim],
-            #    a=-0.0000001,
-            #    b=0.0000001
-            # )
+            nn.init.uniform_(
+               tensor=self.entity_embedding[:, :2 * self.hidden_dim],
+               a=-0.0000001,
+               b=0.0000001
+            )
 
             # nn.init.constant_(
             #     tensor=self.entity_embedding[:, :self.hidden_dim],
@@ -2345,7 +2345,7 @@ class KGEModel(nn.Module):
         if args.negative_adversarial_sampling:
             # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
             # detach() 函数起到了阻断backpropogation的作用
-            negative_score = (F.softmax(negative_score * args.adversarial_temperature, dim=1).detach()
+            negative_score = (F.softmax(N_inference * args.adversarial_temperature, dim=1).detach()
                               * F.logsigmoid(- negative_score)).sum(dim=1)
 
         else:
