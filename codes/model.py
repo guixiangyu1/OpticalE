a3c1c777f8f75d18318cf1b676a7bb11b2d559ba
guixiangyu <guixiangyu1@gmail.com>
2020-11-10 14:49:24 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -585,8 +585,8 @@ class KGEModel(nn.Module):
         inference = torch.abs(torch.cos(head3 - tail3))
 
         if mode == 'head-batch' or mode == 'tail-batch':
-            head1 = head1.detach()
-            tail1 = tail1.detach()
+            head1 = torch.max(head1, tail1)
+            tail1 = torch.min(head1, tail1).detach()
 
         intensity = head1 ** 2 + tail1 ** 2 + 2 * head1 * tail1 * torch.cos(head2 + rel2 - tail2) * inference
         score = self.gamma.item() - intensity.sum(dim=2)
