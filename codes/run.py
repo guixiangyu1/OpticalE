c6e5e05821ade8cce210662e5a124feb4716b8a4
guixiangyu <guixiangyu1@gmail.com>
2020-12-16 12:10:48 +0800

    fix bug

--- codes/run.py
+++ codes/run.py
@@ -16,10 +16,10 @@ import torch
 
 from torch.utils.data import DataLoader
 
-from .model import KGEModel
+from model import KGEModel
 
-from .dataloader import TrainDataset
-from .dataloader import BidirectionalOneShotIterator
+from dataloader import TrainDataset
+from dataloader import BidirectionalOneShotIterator
 from .gcnModels import GCN
 
 def parse_args(args=None):
