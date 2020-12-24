801abd02e8f2743125faa0ce53792234eca12fa3
guixiangyu <guixiangyu1@gmail.com>
2020-12-24 20:21:23 +0800

    test

--- codes/model.py
+++ codes/model.py
@@ -2353,7 +2353,7 @@ class KGEModel(nn.Module):
         (negative_score, N_a), N_inference = model((positive_sample, negative_sample), mode=mode)
         (positive_score, P_a), P_inference = model(positive_sample)
 
-        negative_score = negative_score - 10
+        negative_score = negative_score - 15
 
         if args.negative_adversarial_sampling:
             # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
