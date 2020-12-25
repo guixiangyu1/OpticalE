8f8eadaa5f54f3b5a4384569574a92c21b0e79ef
guixiangyu <guixiangyu1@gmail.com>
2020-12-25 12:46:15 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -487,8 +487,8 @@ class KGEModel(nn.Module):
 
         intensity = head1 ** 2 + tail1 ** 2 + 2.0 * head1 * tail1 * (a * inference)
 
-        # intensity = (intensity + 0.000001) ** 1.1
-        score = self.gamma.item() - intensity.sum(dim=2)
+        # intensity = (intensity + 0.000001) ** 0.5
+        score = self.gamma.item() - intensity.sum(dim=2) ** 0.5
 
         return (score, a), inference.mean(dim=2)
 
