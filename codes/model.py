54ea96c81801abd6b4836f311eabfae6362ec02f
guixiangyu <guixiangyu1@gmail.com>
2020-12-25 17:53:02 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1597,8 +1597,7 @@ class KGEModel(nn.Module):
         score = (intensity_h + intensity_t) + interference
         # print((self.relGamma * 10))
 
-        gamma = gamma.abs() / self.relGamma.abs().sum() * 4 + self.gamma.item()
-
+        gamma = gamma.abs() / self.relGamma.abs().sum() * 2 + self.gamma.item()
 
         score = gamma - score.sum(dim=2)
 
