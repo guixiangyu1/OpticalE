f74348d36701bd75b04c22ef33806d01ac6ecb75
guixiangyu <guixiangyu1@gmail.com>
2020-12-26 20:44:05 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -39,7 +39,7 @@ class KGEModel(nn.Module):
 
         # 初始化embedding TestE
         self.embedding_range = nn.Parameter(
-            torch.Tensor([(9.0 + self.epsilon) / hidden_dim]),
+            torch.Tensor([(12.0 + self.epsilon) / hidden_dim]),
             requires_grad=False
         )
 
@@ -54,7 +54,7 @@ class KGEModel(nn.Module):
         )
 
         self.mod_range = nn.Parameter(
-            torch.Tensor([((9.0 + self.epsilon) * 0.5 / hidden_dim) ** 0.5]),
+            torch.Tensor([((12.0 + self.epsilon) * 0.5 / hidden_dim) ** 0.5]),
             requires_grad=False
         )
 
