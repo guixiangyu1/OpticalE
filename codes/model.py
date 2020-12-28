3b347b09963e4f4a0557b534e2cb512ac2d6b2a9
guixiangyu <guixiangyu1@gmail.com>
2020-12-28 12:33:40 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -54,7 +54,7 @@ class KGEModel(nn.Module):
         )
 
         self.mod_range = nn.Parameter(
-            torch.Tensor([((self.gamma.item() + self.epsilon) * 0.5 / hidden_dim) ** 0.5]),
+            torch.Tensor([((self.gamma.item()+8.0 + self.epsilon) * 0.5 / hidden_dim) ** 0.5]),
             requires_grad=False
         )
 
