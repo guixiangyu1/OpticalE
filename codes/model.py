6097a05232285514a7ce2ee98f028b2b36bffbbb
guixiangyu <guixiangyu1@gmail.com>
2021-01-04 20:20:58 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1735,7 +1735,7 @@ class KGEModel(nn.Module):
 
         intensity = 2 * a + 2
 
-        weight = torch.sigmoid(20 * weight)
+        weight = torch.sigmoid(50 * weight)
         weight = torch.relu(500 - weight.sum(dim=2, keepdims=True)) * F.normalize((1 - weight), p=1, dim=2) + weight
         score = self.gamma.item() - (intensity * weight).sum(dim=2) * 0.008 / weight.sum(dim=2) * 500
 
