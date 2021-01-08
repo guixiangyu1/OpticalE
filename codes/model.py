9a8af44f49fb25ac6077ffa21ae1354cdffcd488
guixiangyu <guixiangyu1@gmail.com>
2021-01-08 15:49:22 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1669,7 +1669,7 @@ class KGEModel(nn.Module):
         # weight = torch.relu(500 - weight.sum(dim=2, keepdims=True)) * F.normalize((1 - weight), p=1, dim=2) + weight
         # print(weight.min())
         # print(weight.max())
-        weight = torch.cat([torch.ones(weight.shape).cuda()[:,:,:300], weight[:,:,300:]], dim=2)
+        weight = torch.cat([torch.ones(weight.shape).cuda()[:,:,:400], weight[:,:,400:]], dim=2)
         print(weight.sum(dim=2)[0 ,0].item())
         bias = 500
 
