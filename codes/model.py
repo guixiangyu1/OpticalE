a59e15a0035c645bf7ca2e647f0066e9a0f5f783
guixiangyu <guixiangyu1@gmail.com>
2021-01-08 10:45:02 +0800

    fix bug

--- codes/model.py
+++ codes/model.py
@@ -1670,8 +1670,9 @@ class KGEModel(nn.Module):
         # print(weight.min())
         # print(weight.max())
         print(weight.sum(dim=2)[0 ,0].item())
+        bias = 200
 
-        score = (self.gamma.item() - (weight * score).sum(dim=2) * 0.008) / weight.sum(dim=2) * self.hidden_dim
+        score = (self.gamma.item() - (weight * score).sum(dim=2) * 0.008) / (weight.sum(dim=2) + bias) * (self.hidden_dim + bias)
         # score = self.gamma.item() - score.sum(dim=2) * 0.008
         return (score, a), torch.Tensor([1])
 
